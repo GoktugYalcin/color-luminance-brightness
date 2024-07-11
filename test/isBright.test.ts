4f481324621a7e62576e02647d2e35c310f95059
Goktug Yalcin <goktug.yalcin@next4biz.com>
2024-07-11 09:00:54 +0300

    fix: Test cases fixed

--- test/isBright.test.ts
+++ test/isBright.test.ts
@@ -7,10 +7,22 @@ describe('luminance brightness decide', () => {
   it('is black bright', () => {
     expect(isBright('#000000')).toEqual(false);
   });
+  it('is white bright(with 3 char)', () => {
+    expect(isBright('#fff')).toEqual(true);
+  });
+  it('is black bright(with 3 char)', () => {
+    expect(isBright('#000')).toEqual(false);
+  });
   it('is white dark', () => {
     expect(isDark('#ffffff')).toEqual(false);
   });
   it('is black dark', () => {
     expect(isDark('#000000')).toEqual(true);
+  });
+  it('is white dark(with 3 char)', () => {
+    expect(isDark('#fff')).toEqual(false);
+  });
+  it('is black dark(with 3 char)', () => {
+    expect(isDark('#000')).toEqual(true);
   });
 });
